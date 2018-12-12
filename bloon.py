78d21acdee2f154773ba66d204b74db5f66e2762
BIGNYAN <45829784+BIGNYAN@users.noreply.github.com>
2018-12-12 19:33:41 +0000

    Add files via upload

---
+++ bloon.py
@@ -0,0 +1,25 @@
+if 5 != 5:
+    print('0')
+if 5 == 5:
+    print('1')
+A = 10
+if A > 10:
+    print('>10')
+if A <= 10:
+    print(A)
+
+if 10 != 1:
+    print('huihui')
+
+if 10 == 10 and 10 != 1:
+    print('ture')
+    
+#########   
+humnag = 'you'
+if 10 == 10 and 1 != 1:
+    print('true')
+elif humnag == 'yu':
+    print('09')
+else:
+    print('8484848484')
+
